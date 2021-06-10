f45527e817f7214ef56ab6668e6199787c0f65e1
James Clark Jr <James.clark.jr@aa.com>
2021-06-10 13:16:26 -0500

    Removed useless code

--- main.tf
+++ main.tf
@@ -34,23 +34,6 @@ resource "azurerm_storage_account" "asom-storage" {
 
 
 /*
-resource "azurerm_function_app" "mocasomfunctionappdev" {
-  name                       = "${var.resprefix}functionapp"
-  location                   = "${data.azurerm_resource_group.asom.location}"
-  resource_group_name        = "${data.azurerm_resource_group.asom.name}"
-  app_service_plan_id        = azurerm_app_service_plan.asom.id
-  storage_account_name       = azurerm_storage_account.asom-storage.name
-  storage_account_access_key = azurerm_storage_account.asom-storage.primary_access_key
-  
-  app_settings = {
-    FUNCTIONS_WORKER_RUNTIME = "node",
-    CosmosDBAuthKey = "9uRWyXfXOuGXhPcuywKacLMctUuSG7QgE7AXO2cKrPtQRpc6lINBwZNdsvqkLVE6h26f6k9Uf5p6JVv30imBtA==",
-    CosmosDBEndpoint ="https://mocasomtestcosmosdb.documents.azure.com:443/",
-    DatabaseName = "mocasomcosmosdbdatabase",
-    CollectionName = "mocasomcosmosdbcontainer",
-  }
-}
-
 resource "azurerm_app_service_plan" "asom" {
   name                = "${var.prefix}-asp"
   location            = "${data.azurerm_resource_group.asom.location}"
@@ -64,4 +47,6 @@ resource "azurerm_app_service_plan" "asom" {
   }
 }
 */
+
+
 
